4abaa92363bff51ab2756cdee7a01f883cb2815a
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2022-04-04 20:39:08 -0300

    added Game component and changes at index.js

---
+++ src/Game.js
@@ -0,0 +1,10 @@
+
+function Game() {
+    return (
+        <div>
+            Hello Wolrd
+        </div>
+    );
+}
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Game from './Game';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game/>
   </React.StrictMode>,
   document.getElementById('root')
 );
